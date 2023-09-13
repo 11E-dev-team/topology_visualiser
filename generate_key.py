dbf3814a511a9f79ceab8d23eec2e0e3812998ea
Sp1dz <spidepog@gmail.com>
2023-09-13 19:30:11 +0300

    Added cryptography

---
+++ generate_key.py
@@ -0,0 +1,14 @@
+from cryptography.fernet import Fernet
+
+def regenerate_key():
+    with open('.secret_key', mode='wb') as key:
+        key.write(Fernet.generate_key())
+
+
+
+if __name__ == '__main__':
+
+    sure = input('Вы уверены, что вы хотите перегенерировать ключ шифрации данных? (д/Н): ') == 'д'
+    if sure:
+        regenerate_key()
+        print('Сгенерирован новый ключ')
--- userdata.py
+++ userdata.py
@@ -1,20 +1,35 @@
+from cryptography.fernet import Fernet
+
+
+def get_key():
+    from os.path import isfile
+    from generate_key import regenerate_key
+    if not isfile('.secret_key'):
+        regenerate_key()
+    with open('.secret_key', mode='rb') as key:
+        return key.read()
+
 USERS_FILE = 'users.csv'
 
 def add_user_data(ip: str, login: str, password: str, flag: str = '-'):
     with open(USERS_FILE, mode='a') as uf:
-        uf.write(f'{ip};{login};{password};{flag}'+'\n')
+        data = f'{ip};{login};{password};{flag}'
+        uf.write(Fernet(get_key()).encrypt(data.encode()).decode()+'\n')
 
 def read_user_data():
     data_by_ip, data_by_flag = {}, {}
     header = True
+    fn = Fernet(get_key())
     with open(USERS_FILE, mode='r') as uf:
         for line in uf.readlines():
             if header: 
                 header = False
                 continue
+            line = fn.decrypt(line.encode()).decode()
             line = line.strip('\n')
             ip, login, password, flag = line.split(';')
             data_by_ip[ip] = data_by_flag[flag] = [ip, login, password, flag]
+    print(data_by_flag)
     return data_by_ip, data_by_flag
 
 def dump_user_data(data):
@@ -22,4 +37,5 @@ def dump_user_data(data):
         uf.write('ip;login;password;flag\n')
         for ip, userdata in data.items():
             _, login, password, flag = userdata
-            uf.write(f'{ip};{login};{password};{flag}')
+            data = f'{ip};{login};{password};{flag}'
+            uf.write(Fernet(get_key()).encrypt(data.encode()).decode()+'\n')
